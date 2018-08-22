0ea8c3b1c8d206db7722a75461e70de13b3f285a
Josue Francisco <josuefrancisco2017@outlook.pt>
2018-08-22 08:50:51 +0100

    Done... script is complite

---
+++ polidroms.php
@@ -0,0 +1,29 @@
+<?php
+
+	/*
+		author: Josué Francisco
+		description: if function polidromss return true, the string $word is a polidrom
+
+		Palindromes-> Are words or phrases that can be read from left to right or from right to left. We can say that the palindrome, compared to the common phrase, is like a round-trip ticket. "Ana," for example, is a palindromic name. see more in https://www.soportugues.com.br/secoes/palindromos/
+	*/
+
+    class Polidrom
+	{
+		public static function polodromss($word){
+			
+			$word = strtolower($word);
+			
+			if($word == strrev($word))
+				return true;
+			else
+				return false;
+		}
+    }
+
+    $word = "Damsmad";
+    echo Polidrom::polodromss($word);
+
+?>
+
+
+	
